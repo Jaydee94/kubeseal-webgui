e91eb26e70b606c9b8471fb9410541ab348cc63d
jaydee94 <janherber@gmx.de>
2020-03-16 20:17:02 +0100

    Add Copy Button

---
+++ static/copy.js
@@ -0,0 +1,13 @@
+function CopyToClipboard(containerid) {
+    if (document.selection) { 
+        var range = document.body.createTextRange();
+        range.moveToElementText(document.getElementById(containerid));
+        range.select().createTextRange();
+        document.execCommand("copy"); 
+    
+    } else if (window.getSelection) {
+        var range = document.createRange();
+         range.selectNode(document.getElementById(containerid));
+         window.getSelection().addRange(range);
+         document.execCommand("copy");
+    }}
