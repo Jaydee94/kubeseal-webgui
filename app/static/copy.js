dafdf7e715a25593c41976d9bdd021edc26e344d
Jan Herber <janherber@gmx.de>
2020-03-19 15:40:27 +0100

    Beautify Files

--- app/static/copy.js
+++ app/static/copy.js
@@ -1,13 +1,14 @@
 function CopyToClipboard(containerid) {
-    if (document.selection) { 
+    if (document.selection) {
         var range = document.body.createTextRange();
         range.moveToElementText(document.getElementById(containerid));
         range.select().createTextRange();
-        document.execCommand("copy"); 
-    
+        document.execCommand("copy");
+
     } else if (window.getSelection) {
         var range = document.createRange();
-         range.selectNode(document.getElementById(containerid));
-         window.getSelection().addRange(range);
-         document.execCommand("copy");
-    }}
+        range.selectNode(document.getElementById(containerid));
+        window.getSelection().addRange(range);
+        document.execCommand("copy");
+    }
+}
