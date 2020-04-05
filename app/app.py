ea2d6cdecf2f44623d9fa768016488c2c38c1278
Johannes Siebel <johannes.siebel@gmail.com>
2020-04-05 13:23:10 +0200

    Mostly UI changes, Added webassets dependency
    Signed-off-by: Johannes Siebel <johannes.siebel@gmail.com>

--- app/app.py
+++ app/app.py
@@ -3,6 +3,8 @@ from flask_wtf import FlaskForm
 from wtforms import TextField
 from wtforms.validators import InputRequired
 from flask_bootstrap import Bootstrap
+from flask import Flask
+from flask_assets import Environment, Bundle
 from kubeseal import Kubeseal
 from os import urandom
 import sys
@@ -10,10 +12,14 @@ import subprocess
 
 app = Flask(__name__)
 Bootstrap(app)
+
+assets = Environment(app)
+less = Bundle('output.css', filters='cssmin', output='screen.css')
+assets.register('css_all', less)
 app.config['SECRET_KEY'] = urandom(24)
   
 # Defining the form
-class KubsealForm(FlaskForm):
+class KubesealForm(FlaskForm):
     cleartextSecret = TextField('Klartext-Secret:', validators=[InputRequired()])
     secretName = TextField('Secret-Name:', validators=[InputRequired()])
     secretNamespace = TextField('Secret-Namespace:', validators=[InputRequired()])            
@@ -21,14 +27,13 @@ class KubsealForm(FlaskForm):
 # Main Page
 @app.route("/", methods=['GET', 'POST'])
 def run_kubeseal():
-    form = KubsealForm()
+    form = KubesealForm()
     if form.validate_on_submit():
         cltSecret = form.cleartextSecret.data
         sName = form.secretName.data
         sNamespace = form.secretNamespace.data
         sealedSecret = Kubeseal.kubectlCMD(cltSecret,sNamespace,sName)
         sys.stdout.write('Created Sealed-Secret %s for namespace %s\n'%(sName,sNamespace))
-        #return output
         return render_template('output.html', sealedSecret=sealedSecret[0])
 
     return render_template('main.html', form=form)
--- app/kubeseal.py
+++ app/kubeseal.py
@@ -4,7 +4,7 @@ class Kubeseal:
 
   @staticmethod
   def kubectlCMD(clearSecret, secretNamespace, secretName):
-    command = "echo -n %s | /app/kubeseal --raw --from-file=/dev/stdin --namespace %s --name %s --cert /app/cert/kubeseal-cert.pem" % (clearSecret, secretNamespace, secretName)
+    command = "echo -n %s | /kubeseal-webgui/kubeseal --raw --from-file=/dev/stdin --namespace %s --name %s --cert /app/cert/kubeseal-cert.pem" % (clearSecret, secretNamespace, secretName)
     test = subprocess.Popen([command], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     output, errors = test.communicate()
     if errors:
