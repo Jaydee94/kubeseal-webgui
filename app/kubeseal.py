04c8ce86fb4b3c78289eac2bb2cbbe9a5437e241
Jan Herber <jaydee@DESKTOP-GTJ767I.localdomain>
2020-04-07 12:23:57 +0200

    Fix pylint errors

--- app/kubeseal.py
+++ app/kubeseal.py
@@ -1,11 +1,13 @@
 import subprocess
+
+
 class Kubeseal:
 
-  @staticmethod
-  def kubectlCMD(clearSecret, secretNamespace, secretName):
-    command = "echo -n '%s' | /kubeseal-webgui/kubeseal --raw --from-file=/dev/stdin --namespace %s --name %s --cert /kubeseal-webgui/cert/kubeseal-cert.pem" % (clearSecret, secretNamespace, secretName)
-    test = subprocess.Popen([command], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-    output, errors = test.communicate()
-    if errors:
-      print(errors[0])
-    return output.decode('utf-8').split('\n')
+    @staticmethod
+    def kubectlCMD(clearSecret, secretNamespace, secretName):
+        command = "echo -n '%s' | /kubeseal-webgui/kubeseal --raw --from-file=/dev/stdin --namespace %s --name %s --cert /kubeseal-webgui/cert/kubeseal-cert.pem" % (clearSecret, secretNamespace, secretName)
+        test = subprocess.Popen([command], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        output, errors = test.communicate()
+        if errors:
+            print(errors[0])
+        return output.decode('utf-8').split('\n')
