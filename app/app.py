85c653f8541893fe265ed91bede93e0badc62539
Jan Herber <jaydee@DESKTOP-GTJ767I.localdomain>
2020-07-08 17:24:59 +0200

    Added the following features
     - Print out complete sealedSecret kubernetes object after encrypting
     - modified the flask logs
     - custom logging after sealedSecret has been created
     - modified the copy.js for dynamic tooltips

--- app/app.py
+++ app/app.py
@@ -6,18 +6,25 @@ from flask_bootstrap import Bootstrap
 from flask_assets import Environment, Bundle
 from kubeseal import Kubeseal
 from os import urandom
+import jinja2
 import sys
+import logging
 
+# Configure logging settings
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+log = logging.getLogger("kubseal-webgui")
+flasklogger = logging.getLogger('werkzeug')
+flasklogger.setLevel(logging.ERROR)
+
+# Initialize flask app including bootstrap
 app = Flask(__name__)
 Bootstrap(app)
-
 assets = Environment(app)
 less = Bundle('output.css', filters='cssmin', output='screen.css')
 assets.register('css_all', less)
 app.config['SECRET_KEY'] = urandom(24)
 
-
-# Defining the form
+# Defining the kubeseal-form
 class KubesealForm(FlaskForm):
     cleartextSecret = TextAreaField('Cleartext secret:', validators=[InputRequired()])
     secretName = TextField('Secret name:', validators=[InputRequired()])
@@ -27,18 +34,24 @@ class KubesealForm(FlaskForm):
 # Main Page
 @app.route("/", methods=['GET', 'POST'])
 def run_kubeseal():
-    """Runs the kubseal command and generates the flask form"""
+    """Runs the kubeseal command and generates the flask form"""
     form = KubesealForm()
+    # Actions after clicking the submit-button
     if form.validate_on_submit():
         cltSecret = form.cleartextSecret.data
         sName = form.secretName.data
         sNamespace = form.secretNamespace.data
         sealedSecret = Kubeseal.kubectlCMD(cltSecret, sNamespace, sName)
-        sys.stdout.write('Created Sealed-Secret %s for namespace %s \n' % (sName, sNamespace))
-        return render_template('output.html', sealedSecret=sealedSecret[0])
+        log.info('Created SealedSecret [%s] for Namespace[%s]' % (sName, sNamespace))
+
+        # Load data from YAML into Python dictionary
+        env = jinja2.Environment(loader = jinja2.FileSystemLoader('./templates'), trim_blocks=True, lstrip_blocks=True)
+        template = env.get_template('sealed-secret.yaml')
+        kubernetesObject = template.render(sealedsecretName=sName, sealedsecretNamespace=sNamespace, encryptedSecret=sealedSecret[0])
+        return render_template('output.html', sealedSecret=sealedSecret[0], kubernetesObject=kubernetesObject)
 
     return render_template('main.html', form=form)
 
 # Main Method
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=False)
