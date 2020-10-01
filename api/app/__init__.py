e783271edda2571628a0de807c6b82086e781d54
Johannes Siebel <johannes.siebel@gmail.com>
2020-10-01 21:58:16 +0200

    Read config.py in case it is needed sometime
    Signed-off-by: Johannes Siebel <johannes.siebel@gmail.com>

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -28,6 +28,13 @@ def create_app(test_config=None):
     """ Initializes Flask application module. """
     app = Flask(__name__)
 
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
     if "ORIGIN_URL" not in environ:
         raise RuntimeError("Error: Environment variable ORIGIN_URL empty.")
 
