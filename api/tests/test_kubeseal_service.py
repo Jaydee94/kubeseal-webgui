6e1739ff559b21aa4af54253790a455967614ae9
Jan Herber <jaydee@DESKTOP-GTJ767I.localdomain>
2021-04-30 18:04:39 +0200

    formatting with black; remove unused imports

--- api/tests/test_kubeseal_service.py
+++ api/tests/test_kubeseal_service.py
@@ -1,4 +1,3 @@
-from app.kubeseal import run_kubeseal
 from os import environ
 import json
 import pytest
@@ -11,9 +10,17 @@ environ["ORIGIN_URL"] = "http://no-server-here"
 @pytest.mark.container
 @pytest.mark.cluster
 def test_post_secrets_object(app, client):
-    sealing_request_data = {'namespace': 'foo', 'secret': 'bar', 'secrets': ['my', 'precious']}
-    expected_sealing_response_data = {'namespace': 'foo', 'secret': 'bar', 'sealedSecrets': ['kauder', 'welsch']}
-    res = client.post('/secrets', data=sealing_request_data)
+    sealing_request_data = {
+        "namespace": "foo",
+        "secret": "bar",
+        "secrets": ["my", "precious"],
+    }
+    expected_sealing_response_data = {
+        "namespace": "foo",
+        "secret": "bar",
+        "sealedSecrets": ["kauder", "welsch"],
+    }
+    res = client.post("/secrets", data=sealing_request_data)
     assert res.status_code == 200
     assert expected_sealing_response_data == json.loads(res.get_data(as_text=True))
 
@@ -21,7 +28,7 @@ def test_post_secrets_object(app, client):
 def test_get_api(client):
     # given running http server
     # when GET /secrets
-    res = client.get('/secrets')
+    res = client.get("/secrets")
     # then return non empty result
     assert res.status_code == 200
     assert res.get_data(as_text=True) != ""
@@ -35,9 +42,11 @@ def test_post_api(client):
     data = {
         "secret": "test-secret",
         "namespace": "test-namespace",
-        "secrets": [{"key": "foo", "value": "bar"}, {"key": "bar", "value": "foo"}]
+        "secrets": [{"key": "foo", "value": "bar"}, {"key": "bar", "value": "foo"}],
     }
-    res = client.post('/secrets', data=json.dumps(data), headers={"Content-Type": "application/json"})
+    res = client.post(
+        "/secrets", data=json.dumps(data), headers={"Content-Type": "application/json"}
+    )
     # then return non empty result
     assert res.status_code == 200
     assert res.get_data(as_text=True) != ""
