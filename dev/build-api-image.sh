77571cc21422c381275d7268168249babff7e7f7
Jan Herber <jaydee@DESKTOP-GTJ767I.localdomain>
2021-05-06 17:56:24 +0200

    Upgrade to version 2.1.1

--- dev/build-api-image.sh
+++ dev/build-api-image.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
 BASEDIR=$(dirname "$0")
-docker build $BASEDIR/.. -f $BASEDIR/../Dockerfile.api -t api:2.1.0 -t kubesealwebgui/api:2.1.0
+docker build $BASEDIR/.. -f $BASEDIR/../Dockerfile.api -t api:2.1.1 -t kubesealwebgui/api:2.1.1
--- dev/local-dev-minikube.sh
+++ dev/local-dev-minikube.sh
@@ -10,12 +10,12 @@ $BASEDIR/build-ui-image.sh
 
 helm template kubesealwebgui  $BASEDIR/../chart/kubeseal-webgui/ | kubectl apply -f -
 
-sleep 2
+sleep 5
 
 NEW_POD=$(kubectl get pods --field-selector status.phase=Pending -o custom-columns=":metadata.name")
 echo " "
 echo "Use the following commands to locally port-forward to the kubesealwebgui pod"
 echo "-------------------------------------------"
-printf "kubectl port-forward ${NEW_POD} -p 5000:5000\n"
-printf "kubectl port-forward ${NEW_POD} -p 8080:8080\n"
+printf "kubectl port-forward ${NEW_POD} 5000:5000\n"
+printf "kubectl port-forward ${NEW_POD} 8080:8080\n"
 echo " "
