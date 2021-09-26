cc5c884a57e946d8d63defce185fa5d87243e3ca
Johannes Siebel <johannes.siebel@gmail.com>
2021-09-26 21:43:34 +0200

    Fixed comment to test pre-commit hook
    Signed-off-by: Johannes Siebel <johannes.siebel@gmail.com>

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -30,7 +30,7 @@ def create_app(test_config=None):
     app = Flask(__name__)
 
     if test_config is None:
-        # load the instance config, if it exists, when not testing
+        # when not testing, load the instance config if it exists
         app.config.from_pyfile("config.py", silent=True)
     else:
         # load the test config if passed in
