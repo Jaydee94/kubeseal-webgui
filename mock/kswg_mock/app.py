496ce0cc07f3a1492a5e890e1fade1a9dd4942d6
Jan Herber <jaydee@DESKTOP-GTJ767I>
2022-10-01 20:07:20 +0200

    setup a local mocking api with fastapi

--- mock/kswg_mock/app.py
+++ mock/kswg_mock/app.py
@@ -1,14 +1,33 @@
 import fastapi
+from fastapi.middleware.cors import CORSMiddleware
 
-from .routers import kubernetes
+from .routers import kubernetes, config, kubeseal
 
 app = fastapi.FastAPI()
 
-app.include_router(
-    kubernetes.router,
-    prefix='/api/v1',
+origins = [
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
-@app.get('/')
+app.include_router(
+    kubernetes.router,
+)
+app.include_router(
+    config.router,
+)
+app.include_router(
+    kubeseal.router,
+)
+
+
+@app.get("/")
 def root():
-    return {'status': 'KubeSeal WebGui Mock'}
+    return {"status": "KubeSeal WebGui Mock"}
--- mock/kswg_mock/routers/kubernetes.py
+++ mock/kswg_mock/routers/kubernetes.py
@@ -1,24 +1,9 @@
 import fastapi
+import json
 
 router = fastapi.APIRouter()
 
-@router.get('/namespaces')
-def get_namespaces():
-    namespace_spec = {}
-    namespace_meta = {
-      'name': 'mock',
-    }
-    namespace = {
-        'kind': 'Namespace',
-        'api_version': 'v1',
-        'metadata': namespace_meta,
-        'spec': namespace_spec,
-    }
-    namespaces = [namespace]
-    namespace_list = {
-        'kind': 'NamespaceList',
-        'api_version': 'v1',
-        'items': namespaces,
-    }
 
-    return namespace_list
+@router.get("/namespaces")
+def get_namespaces():
+    return json.dumps(["foo", "bar"])
