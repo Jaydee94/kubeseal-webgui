c381b123918580fa4590beaf2a09f06bc451dd86
Felix Schumacher <felix.schumacher@internetallee.de>
2022-10-25 12:13:09 +0200

    make linter even happier

--- ui/vue.config.js
+++ ui/vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   pluginOptions: {
     vuetify: {
-			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
-		}
+      // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+    }
   }
 })
