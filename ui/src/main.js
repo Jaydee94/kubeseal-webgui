f9d3734f0b8359f34556e2e798cb97d51f9a2159
Felix Schumacher <felix.schumacher@internetallee.de>
2022-10-25 18:52:55 +0200

    Use locally provided roboto font

--- ui/src/main.js
+++ ui/src/main.js
@@ -2,14 +2,13 @@ import 'vuetify/styles' // Global CSS has to be imported
 import { createApp } from 'vue'
 import { createVuetify } from 'vuetify'
 import App from './App.vue'
-import { loadFonts } from './plugins/webfontloader'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import { aliases, mdi } from 'vuetify/iconsets/mdi'
 
+import "@fontsource/roboto"
 import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
 
-loadFonts()
 const app = createApp(App)
 const vuetify = createVuetify({
   components,
