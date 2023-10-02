a23219429c29fa55c7b11d8fc2c3492159d1a80d
Felix Schumacher <felix.schumacher@internetallee.de>
2023-10-02 18:20:42 +0200

    Convert AppConfig to components (#224)
    and hopefully simplify the code by it.

--- ui/src/components/AppConfig.vue
+++ ui/src/components/AppConfig.vue
@@ -5,15 +5,15 @@
         v-if="fetchConfigsSuccessful"
         class="text-muted"
       >
-        <span
+        <template
           v-for="(value, key, index) in configs"
           :key="key"
         >
           <span v-if="index != 0"> ∙ </span><span>{{ key }}: {{ value }}</span>
-        </span>
+        </template>
       </small>
       <small
-        v-else-if="fetchConfigsSuccessful == false"
+        v-else-if="fetchConfigsSuccessful === false"
         class="text-muted"
       >
         ⚠️ Could not retrieve application properties.
@@ -32,47 +32,33 @@
   </v-footer>
 </template>
 
-<script>
-export default {
-  name: "AppConfig",
-  data: function () {
-    return {
-      configs: {},
-      errorMessage: "",
-      fetchConfigsSuccessful: null,
-    };
-  },
-  mounted() {
-    this.fetchConfigs();
-  },
-  methods: {
-    fetchConfigs: async function () {
-      try {
-        let response_config = await fetch("/config.json");
-        let data_config = await response_config.clone().json();
-        let kubeseal_webgui_ui_version =
-          data_config["kubeseal_webgui_ui_version"];
-        let kubeseal_webgui_api_version =
-          data_config["kubeseal_webgui_api_version"];
-        let data = await response_config.clone().json();
-        let apiUrl = data["api_url"];
+<script setup>
+import { ref, onMounted } from 'vue'
+const configs = ref({})
+const errorMessage = ref("")
+const fetchConfigsSuccessful = ref(null)
 
-        let response = await fetch(`${apiUrl}/config`);
-        this.code2 = response.ok;
-        if (response.ok && response_config.ok) {
-          this.fetchConfigsSuccessful = true;
-        } else {
-          this.fetchConfigsSuccessful = false;
-        }
-        let configs = await response.json();
-        this.configs = configs;
-        this.configs.kubeseal_webgui_ui_version = kubeseal_webgui_ui_version;
-        this.configs.kubeseal_webgui_api_version = kubeseal_webgui_api_version;
-      } catch (error) {
-        this.errorMessage = error;
-      }
-    },
-  },
+onMounted(async () => fetchConfigs())
+
+async function fetchConfigs() {
+  try {
+    let response_config = await fetch("/config.json");
+    let data_config = await response_config.clone().json();
+    let kubeseal_webgui_ui_version =
+      data_config["kubeseal_webgui_ui_version"];
+    let kubeseal_webgui_api_version =
+      data_config["kubeseal_webgui_api_version"];
+    let data = await response_config.clone().json();
+    let apiUrl = data["api_url"];
+
+    let response = await fetch(`${apiUrl}/config`);
+    fetchConfigsSuccessful.value = (response.ok && response_config.ok)
+    configs.value = await response.json();
+    configs.value.kubeseal_webgui_ui_version = kubeseal_webgui_ui_version;
+    configs.value.kubeseal_webgui_api_version = kubeseal_webgui_api_version;
+  } catch (error) {
+    errorMessage.value = error;
+  }
 };
 </script>
 
