7f5aea2d180adb5c16afd2f2745af1ebeacda1b0
Jaydee94 <janherber@gmx.de>
2025-11-30 12:11:27 +0100

    feat: Initialize new UI project with Vite, update UI tests for title and list item selection, and adjust SecretFormInputs layout.

--- ui/src/components/SecretFormInputs.vue
+++ ui/src/components/SecretFormInputs.vue
@@ -1,105 +1,109 @@
 <template>
-  <v-row>
-    <v-col
-      cols="12"
-      sm="6"
-      md="5"
-    >
-      <v-autocomplete
-        id="namespaceSelection"
-        :model-value="namespaceName"
-        :items="sortedNamespaces"
-        label="Namespace name"
-        variant="outlined"
-        class="modern-input"
-        color="primary"
-        :disabled="['strict', 'namespace-wide'].indexOf(scope) === -1"
-        @update:model-value="$emit('update:namespaceName', $event)"
+  <div>
+    <v-row justify="end">
+      <v-col
+        cols="12"
+        md="2"
       >
-        <template #item="{ props: itemProps, item }">
-          <v-list-item
-            v-bind="itemProps"
-            :value="item"
-            class="transition-fast"
-          >
-            <template #prepend>
-              <v-icon
-                small
-                color="warning"
-                class="transition-fast"
-                @click.stop="$emit('toggle-favorite', item.value)"
-              >
-                {{ favoriteNamespaces.has(item.value) ? 'mdi-heart' : 'mdi-heart-outline' }}
-              </v-icon>
-            </template>
-          </v-list-item>
-          <v-divider v-if="item.value === lastFavoriteNamespace" :thickness="2" class="my-2"></v-divider>
-        </template>
-        <template #append-inner>
-          <v-tooltip location="bottom" max-width="300" open-on-click>
-            <template #activator="{ props: activatorProps }">
-              <v-icon v-bind="activatorProps" icon="mdi-information-outline" size="small" color="medium-emphasis" class="ml-2" />
-            </template>
-            Select the target namespace where the sealed secret will be deployed.
-          </v-tooltip>
-        </template>
-      </v-autocomplete>
-    </v-col>
-    <v-col
-      cols="12"
-      sm="6"
-      md="5"
-    >
-      <v-text-field
-        id="secretName"
-        :model-value="secretName"
-        label="Secret name"
-        trim
-        clearable
-        variant="outlined"
-        class="modern-input"
-        color="primary"
-        :rules="rules.validDnsSubdomain"
-        :error-messages="secretNameError"
-        :disabled="['strict'].indexOf(scope) === -1"
-        @update:model-value="$emit('update:secretName', $event)"
+        <v-select
+          :model-value="scope"
+          :items="scopes"
+          :select-size="1"
+          :plain="true"
+          label="Scope"
+          variant="outlined"
+          class="modern-input"
+          color="primary"
+          @update:model-value="$emit('update:scope', $event)"
+        >
+          <template #append-inner>
+            <v-tooltip location="bottom" max-width="300" open-on-click>
+              <template #activator="{ props: activatorProps }">
+                <v-icon v-bind="activatorProps" icon="mdi-information-outline" size="small" color="medium-emphasis" class="ml-2" />
+              </template>
+              <span>Specify scope of the secret.<br><i>Scopes for sealed secrets</i></span>
+            </v-tooltip>
+          </template>
+        </v-select>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col
+        cols="12"
+        sm="6"
+        md="6"
       >
-        <template #append-inner>
-          <v-tooltip location="bottom" max-width="300" open-on-click>
-            <template #activator="{ props: activatorProps }">
-              <v-icon v-bind="activatorProps" icon="mdi-information-outline" size="small" color="medium-emphasis" class="ml-2" />
-            </template>
-            <span>Specify name of the secret.<br><i>The secret name must be of type: DNS Subdomain</i></span>
-          </v-tooltip>
-        </template>
-      </v-text-field>
-    </v-col>
-    <v-col
-      cols="12"
-      md="2"
-    >
-      <v-select
-        :model-value="scope"
-        :items="scopes"
-        :select-size="1"
-        :plain="true"
-        label="Scope"
-        variant="outlined"
-        class="modern-input"
-        color="primary"
-        @update:model-value="$emit('update:scope', $event)"
+        <v-autocomplete
+          id="namespaceSelection"
+          :model-value="namespaceName"
+          :items="sortedNamespaces"
+          label="Namespace name"
+          variant="outlined"
+          class="modern-input"
+          color="primary"
+          :disabled="['strict', 'namespace-wide'].indexOf(scope) === -1"
+          @update:model-value="$emit('update:namespaceName', $event)"
+        >
+          <template #item="{ props: itemProps, item }">
+            <v-list-item
+              v-bind="itemProps"
+              :value="item"
+              class="transition-fast"
+            >
+              <template #prepend>
+                <v-icon
+                  small
+                  color="warning"
+                  class="transition-fast"
+                  @click.stop="$emit('toggle-favorite', item.value)"
+                >
+                  {{ favoriteNamespaces.has(item.value) ? 'mdi-heart' : 'mdi-heart-outline' }}
+                </v-icon>
+              </template>
+            </v-list-item>
+            <v-divider v-if="item.value === lastFavoriteNamespace" :thickness="2" class="my-2"></v-divider>
+          </template>
+          <template #append-inner>
+            <v-tooltip location="bottom" max-width="300" open-on-click>
+              <template #activator="{ props: activatorProps }">
+                <v-icon v-bind="activatorProps" icon="mdi-information-outline" size="small" color="medium-emphasis" class="ml-2" />
+              </template>
+              Select the target namespace where the sealed secret will be deployed.
+            </v-tooltip>
+          </template>
+        </v-autocomplete>
+      </v-col>
+      <v-col
+        cols="12"
+        sm="6"
+        md="6"
       >
-        <template #append-inner>
-          <v-tooltip location="bottom" max-width="300" open-on-click>
-            <template #activator="{ props: activatorProps }">
-              <v-icon v-bind="activatorProps" icon="mdi-information-outline" size="small" color="medium-emphasis" class="ml-2" />
-            </template>
-            <span>Specify scope of the secret.<br><i>Scopes for sealed secrets</i></span>
-          </v-tooltip>
-        </template>
-      </v-select>
-    </v-col>
-  </v-row>
+        <v-text-field
+          id="secretName"
+          :model-value="secretName"
+          label="Secret name"
+          trim
+          clearable
+          variant="outlined"
+          class="modern-input"
+          color="primary"
+          :rules="rules.validDnsSubdomain"
+          :error-messages="secretNameError"
+          :disabled="['strict'].indexOf(scope) === -1"
+          @update:model-value="$emit('update:secretName', $event)"
+        >
+          <template #append-inner>
+            <v-tooltip location="bottom" max-width="300" open-on-click>
+              <template #activator="{ props: activatorProps }">
+                <v-icon v-bind="activatorProps" icon="mdi-information-outline" size="small" color="medium-emphasis" class="ml-2" />
+              </template>
+              <span>Specify name of the secret.<br><i>The secret name must be of type: DNS Subdomain</i></span>
+            </v-tooltip>
+          </template>
+        </v-text-field>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script setup>
